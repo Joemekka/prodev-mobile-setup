e3a64f64cda4e1ce233b678c21e42a23e4f8da71
Joemekka <mekka988@gmail.com>
2025-10-30 22:25:38 +0100

    New

---
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text>Entry Screen - Awesome</Text>
+    </View>
+  );
+}
